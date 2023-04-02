57302d4393d8ee754f07dd052f3a47f179924f4b
William_Bryan-Askay <122615154+Fir3atWillBA@users.noreply.github.com>
2023-04-02 15:41:20 +0100

    Update unit_tests.py

--- unit_tests.py
+++ unit_tests.py
@@ -1,33 +1,65 @@
 import unittest
+from tkinter import Tk
 from calculator_code import Calculator
+
 
 class TestCalculator(unittest.TestCase):
     
     def setUp(self):
-        self.calculator = Calculator()
+        self.root = Tk()
+        self.app = Calculator()
+        self.app.total_label = self.app.label = None # Remove label creation from constructor
+        self.app.create_display_labels() # Create labels manually
+        self.app.buttons_frame = self.app.create_buttons_frame()
+        self.app.create_digit_buttons()
+        self.app.create_operator_buttons()
+        self.app.create_special_buttons()
+        self.app.bind_keys()
+
+    def test_smoke(self):
+        self.app.add_to_expression(2)
+        self.app.append_operator("+")
+        self.app.add_to_expression(3)
+        self.app.evaluate()
+        self.assertEqual(self.app.current_expression, "5")
 
     def test_addition(self):
-        result = self.calculator.calculate("5 + 3")
-        self.assertEqual(result, 8)
+        self.app.add_to_expression(5)
+        self.app.append_operator("+")
+        self.app.add_to_expression(3)
+        self.app.evaluate()
+        self.assertEqual(self.app.current_expression, "8")
 
     def test_subtraction(self):
-        result = self.calculator.calculate("5 - 3")
-        self.assertEqual(result, 2)
+        self.app.add_to_expression(5)
+        self.app.append_operator("-")
+        self.app.add_to_expression(3)
+        self.app.evaluate()
+        self.assertEqual(self.app.current_expression, "2")
 
     def test_multiplication(self):
-        result = self.calculator.calculate("5 * 3")
-        self.assertEqual(result, 15)
+        self.app.add_to_expression(5)
+        self.app.append_operator("*")
+        self.app.add_to_expression(3)
+        self.app.evaluate()
+        self.assertEqual(self.app.current_expression, "15")
 
     def test_division(self):
-        result = self.calculator.calculate("10 / 2")
-        self.assertEqual(result, 5)
+        self.app.add_to_expression(10)
+        self.app.append_operator("/")
+        self.app.add_to_expression(2)
+        self.app.evaluate()
+        self.assertEqual(self.app.current_expression, "5")
 
-    def test_complex_expression(self):
-        result = self.calculator.calculate("5 + 3 * 2 - 6 / 3")
-        self.assertEqual(result, 10)
+    def test_clear(self):
+        self.app.add_to_expression(10)
+        self.app.clear()
+        self.assertEqual(self.app.current_expression, "")
+        self.assertEqual(self.app.total_expression, "")
 
     def tearDown(self):
-        del self.calculator
+        self.root.destroy()
 
 if __name__ == '__main__':
     unittest.main()
+
